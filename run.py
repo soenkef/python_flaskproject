b226681cee54d7329508be986afd62fbc3462abc
soenkef <mantu@gmx.de>
2020-10-02 21:54:56 +0200

    Flask-Projekt - Integration von get requests, inhalte fürs template

--- run.py
+++ run.py
@@ -1,16 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 print(__name__)
 app = Flask(__name__)
+
+class Item():
+    def __init__(self, name, amount):
+        self.name = name
+        self.amount = amount
+
 
 # decorator
 @app.route('/')
 def hello_world():
-    items = ["Apfel", "Birne", "Banana"]
+    #items = [
+    #    Item("Apfel", 5),
+    #    Item("Computer", 1),
+    #    Item("Birne", 4)
+    #]
+    items = [
+        {"name": "Apfel", "amount": 5},
+        {"name": "Computer", "amount": 1},
+        {"name": "Birne", "amount": 4}
+    ]#
 
-    return render_template("start.html", name="Max Mustermann", items=items)
+    # Berechnungen - idealerweise nicht im template
+    for item in items:
+        item["amount"] = item["amount"] * 2
+
+    person = ("Max", "Mustermann")
+
+
+    return render_template("start.html", person=person, items=items)
 
 @app.route('/test')
 def test():
-    return render_template("test.html", name="Max Mustermann")
+    name = request.args.get("name")
+    age2 = request.args.get("age")
+    return render_template("test.html", name=name, age=age2)
 
 
